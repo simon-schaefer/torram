fix: jacobians with correct shape

tests/test_geometry_jacobians.py
@@ -1,6 +1,7 @@
 import kornia
 import torch
 
+import torram.geometry
 from torram.geometry import pose_to_transformation_matrix
 from torram.geometry import jacobians as torram_jacobians
 
@@ -11,29 +12,73 @@ def get_random_transform():
     return pose_to_transformation_matrix(t, q)
 
 
-def test_jacobian_transform_tq4d(delta: float = 1e-6):
-    x = torch.rand(3 + 4, dtype=torch.float64)
-    J_hat = torram_jacobians.T_tq4d(x[:3], x[3:])
-    T = pose_to_transformation_matrix(x[:3], x[3:])
-    for k in range(3 + 4):
+def test_T_wrt_t(delta: float = 1e-6):
+    def T_from_t(t: torch.Tensor) -> torch.Tensor:
+        out = torch.eye(4, dtype=t.dtype, device=t.device)
+        out[:3, 3] = t
+        return out
+
+    x = torch.rand(3, dtype=torch.float64)
+    J_hat = torram_jacobians.T_wrt_t(x)
+    T = T_from_t(x)
+    for i in range(3):
         x_ = x.clone()
-        x_[k] += delta
-        Td = pose_to_transformation_matrix(x_[:3], x_[3:])
-        J = (Td - T) / delta
-        assert torch.allclose(J, J_hat[k], atol=1e-4)
+        x_[i] += delta
+        T_ = T_from_t(x_)
+        J = (T_ - T) / delta
+        assert torch.allclose(J_hat[:, i].view(4, 4), J, atol=1e-5)
 
 
-def test_jacobian_tq4d_transform(delta: float = 1e-6):
+def test_T_wrt_q4d(delta: float = 1e-6):
+    def T_from_q(q: torch.Tensor) -> torch.Tensor:
+        out = torch.eye(4, dtype=q.dtype, device=q.device)
+        out[:3, :3] = torram.geometry.quaternion_to_rotation_matrix(q)
+        return out
+
+    x = torch.rand(4, dtype=torch.float64)
+    J_hat = torram_jacobians.T_wrt_q4d(x)
+    T = T_from_q(x)
+    for i in range(4):
+        x_ = x.clone()
+        x_[i] += delta
+        T_ = T_from_q(x_)
+        J = (T_ - T) / delta
+        assert torch.allclose(J_hat[:, i].view(4, 4), J, atol=1e-5)
+
+
+def test_t_wrt_T(delta: float = 1e-6):
     x = get_random_transform()
-    xq = kornia.geometry.rotation_matrix_to_quaternion(x[:3, :3].contiguous())
-
-    J_hat = torram_jacobians.tq4d_T(x)
+    J_hat = torram_jacobians.t_wrt_T(x).view(3, 4, 4)
     for i in range(4):
         for j in range(4):
             x_ = x.clone()
             x_[i, j] += delta
-            q = kornia.geometry.rotation_matrix_to_quaternion(x_[:3, :3].contiguous())
-            Jt = (x_[:3, 3] - x[:3, 3]) / delta
-            Jq = (q - xq) / delta
-            assert torch.allclose(Jt, J_hat[:3, i, j])
-            assert torch.allclose(Jq, J_hat[3:, i, j])
+            J = (x_[:3, 3] - x[:3, 3]) / delta
+            assert torch.allclose(J_hat[:, i, j], J, atol=1e-5)
+
+
+def test_q4d_wrt_T(delta: float = 1e-6):
+    x = get_random_transform()
+    q = kornia.geometry.rotation_matrix_to_quaternion(x[:3, :3].contiguous())
+    J_hat = torram_jacobians.q4d_wrt_T(x).view(4, 4, 4)
+    for i in range(4):
+        for j in range(4):
+            x_ = x.clone()
+            x_[i, j] += delta
+            q_ = kornia.geometry.rotation_matrix_to_quaternion(x_[:3, :3].contiguous())
+            J = (q_ - q) / delta
+            assert torch.allclose(J_hat[:, i, j], J, atol=1e-4)
+
+
+def test_T_inv_wrt_T(delta: float = 1e-6):
+    x = get_random_transform()
+    J_hat = torram_jacobians.T_inv_wrt_T(x).view(16, 4, 4)
+    x_inv = torram.geometry.inverse_transformation(x)
+    for i in range(4):
+        for j in range(4):
+            x_ = x.clone()
+            x_[i, j] += delta
+            x_inv_ = torram.geometry.inverse_transformation(x_)
+            J = (x_inv_ - x_inv) / delta
+            J = torch.flatten(J)
+            assert torch.allclose(J, J_hat[:, i, j])

torram/geometry/jacobians.py
@@ -1,20 +1,26 @@
 import torch
 
 
-__all__ = ['T_tq4d',
-           'tq4d_T',
-           'static_transform']
+__all__ = ['T_wrt_t',
+           't_wrt_T',
+           'T_wrt_q4d',
+           'q4d_wrt_T',
+           'T_inv_wrt_T']
 
 
-def T_tq4d(t: torch.Tensor, q4d: torch.Tensor) -> torch.Tensor:
-    J = torch.zeros((7, 4, 4), dtype=t.dtype, device=t.device)
+def T_wrt_t(t: torch.Tensor) -> torch.Tensor:
+    J = torch.zeros((16, 3), dtype=t.dtype, device=t.device)
+    J[3, 0] = 1
+    J[7, 1] = 1
+    J[11, 2] = 1
+    return J
+
+
+def T_wrt_q4d(q4d: torch.Tensor) -> torch.Tensor:
+    J = torch.zeros((4, 4, 4), dtype=q4d.dtype, device=q4d.device)
     qx, qy, qz, qw = q4d
 
-    J[0, 0, 3] = 1
-    J[1, 1, 3] = 1
-    J[2, 2, 3] = 1
-
-    J[3, :3, :3] = torch.tensor(
+    J[:3, :3, 0] = torch.tensor(
         [[4.0*qx*(qy**2 + qz**2),
           4.0*qx*(qw*qz - qx*qy) + 2.0*qy*(qw**2 + qx**2 + qy**2 + qz**2),
           -4.0*qx*(qw*qy + qx*qz) + 2.0*qz*(qw**2 + qx**2 + qy**2 + qz**2)],
@@ -25,7 +31,7 @@ def T_tq4d(t: torch.Tensor, q4d: torch.Tensor) -> torch.Tensor:
           2.0*qw*(qw**2 + qx**2 + qy**2 + qz**2) - 4.0*qx*(qw*qx + qy*qz),
           4.0*qx*(-qw**2 - qz**2)]]) / (qw**2 + qx**2 + qy**2 + qz**2)**2
 
-    J[4, :3, :3] = torch.tensor(
+    J[:3, :3, 1] = torch.tensor(
         [[4.0*qy*(-qw**2 - qx**2),
           2.0*qx*(qw**2 + qx**2 + qy**2 + qz**2) + 4.0*qy*(qw*qz - qx*qy),
           2.0*qw*(qw**2 + qx**2 + qy**2 + qz**2) - 4.0*qy*(qw*qy + qx*qz)],
@@ -36,7 +42,7 @@ def T_tq4d(t: torch.Tensor, q4d: torch.Tensor) -> torch.Tensor:
           -4.0*qy*(qw*qx + qy*qz) + 2.0*qz*(qw**2 + qx**2 + qy**2 + qz**2),
           4.0*qy*(-qw**2 - qz**2)]]) / (qw**2 + qx**2 + qy**2 + qz**2)**2
 
-    J[5, :3, :3] = torch.tensor(
+    J[:3, :3, 2] = torch.tensor(
         [[4.0*qz*(-qw**2 - qx**2),
           -2.0*qw*(qw**2 + qx**2 + qy**2 + qz**2) + 4.0*qz*(qw*qz - qx*qy),
           2.0*qx*(qw**2 + qx**2 + qy**2 + qz**2) - 4.0*qz*(qw*qy + qx*qz)],
@@ -47,7 +53,7 @@ def T_tq4d(t: torch.Tensor, q4d: torch.Tensor) -> torch.Tensor:
           2.0*qy*(qw**2 + qx**2 + qy**2 + qz**2) - 4.0*qz*(qw*qx + qy*qz),
           4.0*qz*(qx**2 + qy**2)]]) / (qw**2 + qx**2 + qy**2 + qz**2)**2
 
-    J[6, :3, :3] = torch.tensor(
+    J[:3, :3, 3] = torch.tensor(
         [[4.0*qw*(qy**2 + qz**2),
           4.0*qw*(qw*qz - qx*qy) - 2.0*qz*(qw**2 + qx**2 + qy**2 + qz**2),
           -4.0*qw*(qw*qy + qx*qz) + 2.0*qy*(qw**2 + qx**2 + qy**2 + qz**2)],
@@ -58,10 +64,11 @@ def T_tq4d(t: torch.Tensor, q4d: torch.Tensor) -> torch.Tensor:
           -4.0*qw*(qw*qx + qy*qz) + 2.0*qx*(qw**2 + qx**2 + qy**2 + qz**2),
           4.0*qw*(qx**2 + qy**2)]]) / (qw**2 + qx**2 + qy**2 + qz**2)**2
 
+    J = torch.flatten(J, end_dim=-2)
     return J
 
 
-def tq4d_T(T: torch.Tensor) -> torch.Tensor:
+def q4d_wrt_T(T: torch.Tensor) -> torch.Tensor:
     # m00, m01, m02, m10, m11, m12, m20, m21, m22 = torch.chunk(T[..., :3, :3], chunks=9, dim=-1)
     m00, m01, m02, m10, m11, m12, m20, m21, m22 = torch.flatten(T[:3, :3])
 
@@ -222,12 +229,59 @@ def tq4d_T(T: torch.Tensor) -> torch.Tensor:
     where_1 = torch.where((m00 > m11) & (m00 > m22), J_cond1, where_2)
     Jq = torch.where(trace > 0.0, J_trace_positive_cond, where_1)
     Jq = torch.permute(Jq, (2, 0, 1))
-    Jt = torch.zeros((3, 4, 4), dtype=T.dtype, device=T.device)
-    Jt[0, 0, 3] = 1
-    Jt[1, 1, 3] = 1
-    Jt[2, 2, 3] = 1
-    return torch.cat([Jt, Jq], dim=0)
+    return torch.flatten(Jq, start_dim=1)
 
 
-def static_transform(sigmas: torch.Tensor, transform: torch.Tensor):
-    return torch.einsum('...ij, ...ij, ...ji->...ij', transform, sigmas, transform)
+def t_wrt_T(T: torch.Tensor) -> torch.Tensor:
+    J = torch.zeros((3, 16), dtype=T.dtype, device=T.device)
+    J[0, 3] = 1
+    J[1, 7] = 1
+    J[2, 11] = 1
+    return J
+
+
+def T_inv_wrt_T(T: torch.Tensor) -> torch.Tensor:
+    r00, r01, r02, r10, r11, r12, r20, r21, r22 = torch.flatten(T[:3, :3])
+    tx, ty, tz = T[:3, 3]
+
+    J = torch.zeros((4, 4, 4, 4), dtype=T.dtype, device=T.device)
+    J[0, 0, 0, 0] = 1  # wrt r00
+    J[0, 3, 0, 0] = -tx
+
+    J[1, 0, 0, 1] = 1  # wrt r01
+    J[1, 3, 0, 1] = -tx
+
+    J[2, 0, 0, 2] = 1  # wrt r02
+    J[2, 3, 0, 2] = -tx
+
+    J[0, 3, 0, 3] = -r00  # wrt tx
+    J[1, 3, 0, 3] = -r01
+    J[2, 3, 0, 3] = -r02
+
+    J[0, 1, 1, 0] = 1  # wrt r10
+    J[0, 3, 1, 0] = -ty
+
+    J[1, 1, 1, 1] = 1  # wrt r11
+    J[1, 3, 1, 1] = -ty
+
+    J[2, 1, 1, 2] = 1  # wrt r12
+    J[2, 3, 1, 2] = -ty
+
+    J[0, 3, 1, 3] = -r10  # wrt ty
+    J[1, 3, 1, 3] = -r11
+    J[2, 3, 1, 3] = -r12
+
+    J[0, 2, 2, 0] = 1  # wrt r20
+    J[0, 3, 2, 0] = -tz
+
+    J[1, 2, 2, 1] = 1  # wrt r21
+    J[1, 3, 2, 1] = -tz
+
+    J[2, 2, 2, 2] = 1  # wrt r22
+    J[2, 3, 2, 2] = -tz
+
+    J[0, 3, 2, 3] = -r20  # wrt tz
+    J[1, 3, 2, 3] = -r21
+    J[2, 3, 2, 3] = -r22
+
+    return J.view(16, 16)