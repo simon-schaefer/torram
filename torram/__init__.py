1ddaec6dd64154aa480c0e6a52a7bad10fbee727
simon-schaefer <simon.k.schaefer@gmail.com>
2022-04-13 20:05:29 +0200

    model evaluation and visualization on dataset

--- torram/__init__.py
+++ torram/__init__.py
@@ -7,4 +7,6 @@ import torram.visualization
 import torram.transforms
 import torram.estimation
 
+from torram.evaluator import evaluate_dataset
 from torram.trainer import Trainer
+from torram.visualizer import visualize_dataset
---
+++ torram/evaluator.py
@@ -0,0 +1,36 @@
+import torch
+import torch.utils.data
+
+from typing import Any, Dict, Protocol
+
+
+class ModelProtocol(Protocol):  # pragma: no cover
+
+    def wbatch(self, batch) -> Any:
+        ...
+
+    def evaluate(self, batch, model_output, reduce_mean: bool) -> Dict[str, torch.Tensor]:
+        ...
+
+
+def evaluate_dataset(model: ModelProtocol, dataset: torch.utils.data.Dataset, device: torch.device = torch.device('cpu'),
+                     batch_size: int = 1, stop_after: int = -1):
+    """Visualize model outputs for dataset.
+
+    Args:
+        model: prediction model (batch prediction and visualization function).
+        dataset: evaluation dataset.
+        device: device used for processing.
+        batch_size: processing batch size.
+        stop_after: stops after n samples, -1 = do not stop.
+    """
+    data_loader = torch.utils.data.DataLoader(dataset, shuffle=False, batch_size=batch_size)
+    results = []
+    for k, batch in enumerate(data_loader):
+        if stop_after != -1 and k > stop_after:
+            break
+        batch = tuple(x.to(device) for x in batch)
+        model_output = model.wbatch(batch)
+        results_k = model.evaluate(batch, model_output, reduce_mean=False)
+        results.append(results_k)
+    return {key: torch.cat([results_k[key] for results_k in results], dim=0) for key in results[0].keys()}
