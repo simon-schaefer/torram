ca99534a65352e24c13f1a56b2202048e9919f66
simon-schaefer <simon.k.schaefer@gmail.com>
2022-05-03 21:26:09 +0200

    minor updates

--- torram/__init__.py
+++ torram/__init__.py
@@ -7,6 +7,4 @@ import torram.visualization
 import torram.transforms
 import torram.estimation
 
-from torram.evaluator import evaluate_dataset
 from torram.trainer import Trainer
-from torram.visualizer import visualize_dataset
