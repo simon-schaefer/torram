dc136cc4793042e1a8a4947c91519c2d7a76f38a
simon-schaefer <simon.k.schaefer@gmail.com>
2022-05-03 23:52:40 +0200

    fix: evaluate/visualize_dataset in __init__.py

--- torram/__init__.py
+++ torram/__init__.py
@@ -8,3 +8,5 @@ import torram.transforms
 import torram.estimation
 
 from torram.trainer import Trainer
+from torram.evaluator import evaluate_dataset
+from torram.visualizer import visualize_dataset
