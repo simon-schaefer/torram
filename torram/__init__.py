5cfa02897a7a3feb1bf0ede0afc563ee5df0c9b4
simon-schaefer <simon.k.schaefer@gmail.com>
2022-05-06 12:56:37 +0200

    timer -> reset method

--- torram/__init__.py
+++ torram/__init__.py
@@ -10,3 +10,5 @@ import torram.estimation
 from torram.trainer import Trainer
 from torram.evaluator import evaluate_dataset
 from torram.visualizer import visualize_dataset
+from torram.evaluator import evaluate_dataset
+from torram.visualizer import visualize_dataset
--- torram/utility/timer.py
+++ torram/utility/timer.py
@@ -4,6 +4,16 @@ import time
 
 
 class Timer:
+    """Tiny profiling class for timings from explicit code calls.
+
+    >>> timer = Timer()
+    >>> for _ in range(10):
+    >>>     x = 1
+    >>>     timer.log_dt("assigned x")
+    >>>     y = 1
+    >>>     timer.log_dt("assigned y")
+    >>> logs = timer.get_and_reset_logs()
+    """
 
     def __init__(self):
         self._logs = collections.defaultdict(list)
@@ -13,10 +23,13 @@ class Timer:
         toc = time.perf_counter()
         dt = toc - self._tic
         self._logs[name].append(dt)
-        self._tic = time.perf_counter()
+        self.reset()
         logging.debug(f"Logged runtime of {name} = {dt:.5f} s")
 
     def get_and_reset_logs(self):
         logs = self._logs.copy()
         self._logs = collections.defaultdict(list)
         return logs
+
+    def reset(self):
+        self._tic = time.perf_counter()
