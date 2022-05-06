589d38d0afd84308ce7bf83a0294c2c5825dadc4
simon-schaefer <simon.k.schaefer@gmail.com>
2022-05-06 16:45:36 +0200

    tensorboardy.py -> log_normal for logging Normal distributions

--- torram/utility/logger/tensorboardy.py
+++ torram/utility/logger/tensorboardy.py
@@ -1,4 +1,5 @@
 import os
+import torch
 import torch.utils.tensorboard
 from typing import Any, Dict
 
@@ -18,3 +19,10 @@ class TensorboardY(torch.utils.tensorboard.SummaryWriter):
     def add_scalar_dict(self, tag: str, scalar_dict: Dict[str, Any], global_step: int, **kwargs):
         for key, value in scalar_dict.items():
             self.add_scalar(f"{tag}/{key}", value, global_step=global_step)
+
+    def log_normal(self, tag: str, dist_y_hat: torch.distributions.Normal, y: torch.Tensor, global_step: int):
+        error = torch.abs(dist_y_hat.mean - y)
+        self.add_scalar(f"{tag}/xi", torch.mean(error ** 2 / dist_y_hat.variance), global_step=global_step)
+        self.add_histogram(f"{tag}/error", error, global_step=global_step)
+        self.add_histogram(f"{tag}/mean", dist_y_hat.mean, global_step=global_step)
+        self.add_histogram(f"{tag}/stddev", dist_y_hat.stddev, global_step=global_step)
