8f9b1fc937f7680ff748a130cb8f17e928ecdcd4
simon-schaefer <simon.k.schaefer@gmail.com>
2022-05-09 11:14:09 +0200

    draw_reprojection with radius
    - replaced draw_keypoints method with torchvision equivalent
    - enabled varying radius for draw_reprojection method
    - testing of draw_reprojection method

--- tests/test_visualization_vision.py
+++ tests/test_visualization_vision.py
@@ -1,38 +1,28 @@
 import os
-import pytest
 import torch
 import torram
-
-from typing import Tuple
-
-
-@pytest.mark.parametrize("image_shape", ((3, 100, 100), (3, 1000, 200), (3, 2, 2)))
-def test_draw_keypoints(image_shape: Tuple[int, int, int]):
-    from torchvision.utils import draw_keypoints
-    image = torch.zeros(image_shape, dtype=torch.uint8)
-    points = torch.stack([torch.randint(0, image_shape[1], size=(4, ), dtype=torch.long),
-                          torch.randint(0, image_shape[2], size=(4, ), dtype=torch.long)], dim=1)
-    color = (123, 3, 41)
-
-    out_hat = torram.visualization.draw_keypoints(image, points, color=color)
-    # The torchvision implementation draws a circle around each keypoint with radius 1. Therefore,
-    # all the neighbors have to be colored, too.
-    neighbors = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-    for i, j in neighbors:
-        points_ij = points.clone()
-        points_ij[:, 0] += i
-        points_ij[:, 1] += j
-        out_hat = torram.visualization.draw_keypoints(out_hat, points_ij, color=color)
-
-    out = draw_keypoints(image, points[None], colors=color, radius=1)
-    assert torch.allclose(out_hat, out)
 
 
 def test_draw_keypoints_outside_points():
     image = torch.zeros((3, 10, 10), dtype=torch.uint8)
     points = torch.tensor([[11, 0], [-1, 1], [-1, -1]], dtype=torch.long)
-    out_hat = torram.visualization.draw_keypoints(image, points)
+    out_hat = torram.visualization.draw_keypoints(image, points[None])
     assert torch.allclose(out_hat, image)
+
+
+def test_draw_reprojection():
+    image = torch.zeros((3, 480, 640), dtype=torch.uint8)
+    _, h, w = image.shape
+    points_3d = torch.rand((10, 3)) * 3
+    points_3d[:, 2] = 10
+    K = torch.tensor([[384, 0, w/2],
+                      [0, 384, h/2],
+                      [0, 0, 1]], dtype=torch.float32)
+
+    points_2d = torram.geometry.project_points(points_3d, camera_matrix=K).long()
+    image_expected = torram.visualization.draw_keypoints(image, points_2d[None], radius=3, colors="red")
+    image_hat = torram.visualization.draw_reprojection(image, points_3d[None], K=K, radius=3, colors="red")
+    assert torch.allclose(image_hat, image_expected)
 
 
 def test_draw_keypoints_weighted():
--- torram/visualization/vision.py
+++ torram/visualization/vision.py
@@ -1,12 +1,10 @@
 import matplotlib
 import kornia
 import torch
-import torram
-import torchvision
 
 import matplotlib.cm as cm
-from torchvision.utils import draw_bounding_boxes, draw_segmentation_masks
-from typing import Tuple
+from torchvision.utils import draw_bounding_boxes, draw_segmentation_masks, draw_keypoints
+from typing import Tuple, Union
 
 
 __all__ = ['draw_bounding_boxes',
@@ -17,52 +15,24 @@ __all__ = ['draw_bounding_boxes',
 
 
 @torch.no_grad()
-def draw_keypoints(image: torch.Tensor, keypoints: torch.Tensor, color: Tuple[int, int, int] = (255, 0, 0)
-                   ) -> torch.Tensor:
-    """Draw keypoints in pixel coordinates in image by coloring the pixel.
-
-    Args:
-        image: base image to draw keypoints in (3, H, W).
-        keypoints: keypoints in pixel coordinates (N, 2).
-        color: keypoint color as RGB tuple (0-255).
-    """
-    if keypoints.ndim != 2 or keypoints.shape[-1] != 2:
-        raise ValueError(f"Keypoints have invalid shape, expected (N, 2), got {keypoints.shape}")
-    if image.dtype != torch.uint8:
-        raise ValueError(f"Invalid image type, expected uint8, got {image.dtype}")
-    if not all(0 <= x <= 255 for x in color):
-        raise ValueError(f"Invalid values in color tuple, must be [0, 255], got {color}")
-
-    # Remove the points that are not in the image.
-    h, w = image.shape[-2:]
-    is_in_image = torram.geometry.is_in_image(keypoints, height=h, width=w)
-    points_in = keypoints[is_in_image, :]
-
-    # Color the remaining points with the given color.
-    out_image = image.clone()
-    for i in range(3):
-        out_image[i, points_in[:, 1], points_in[:, 0]] = color[i]
-    return out_image
-
-
-@torch.no_grad()
-def draw_reprojection(pc_C: torch.Tensor, image: torch.Tensor, K: torch.Tensor,
-                      color: Tuple[int, int, int] = (255, 0, 0)) -> torch.Tensor:
+def draw_reprojection(image: torch.Tensor, pc_C: torch.Tensor, K: torch.Tensor,
+                      colors: Union[str, Tuple[int, int, int]] = (255, 0, 0), radius: int = 2) -> torch.Tensor:
     """Re-Project a point cloud in the camera frame to the image plane and draw the points.
 
     Args:
         pc_C: point cloud in camera frame (N, 3).
         image: base image to color pixels in.
         K: camera intrinsics for re-projection (3, 3).
-        color: color of colored pixels, either as RGB tuple or hex color (uniform color).
+        colors: color of colored pixels, either as RGB tuple or hex color (uniform color).
+        radius: radius of drawn keypoints.
     """
-    if pc_C.ndim != 2 or pc_C.shape[-1] != 3:
+    if pc_C.ndim != 3 or pc_C.shape[-1] != 3:
         raise ValueError(f"Point clouds have invalid shape, expected (B, N, 3), got {pc_C.shape}")
     if K.shape != (3, 3):
         raise ValueError(f"Intrinsics have invalid shape, expected (3, 3), got {K.shape}")
     K_point_cloud = K[None, :, :]
     pc_projections = kornia.geometry.project_points(pc_C, camera_matrix=K_point_cloud).long()  # int image coordinates
-    return draw_keypoints(image.detach().cpu(), pc_projections, color=color)
+    return draw_keypoints(image.detach().cpu(), pc_projections, colors=colors, radius=radius)
 
 
 @torch.no_grad()
@@ -92,5 +62,5 @@ def draw_keypoints_weighted(image: torch.Tensor, keypoints: torch.Tensor, scores
     color_map = cm.ScalarMappable(norm=norm, cmap=cm.get_cmap(colormap))
     for keypoint_k, score_k in zip(keypoints, scores):
         color_k = color_map.to_rgba(float(score_k), bytes=True)[:3]  # RGBA -> RGB
-        image = torchvision.utils.draw_keypoints(image, keypoint_k[None, None], colors=color_k, radius=radius)
+        image = draw_keypoints(image, keypoint_k[None, None], colors=color_k, radius=radius)
     return image
