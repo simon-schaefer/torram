fix: R->axis-angle numerically stabilized

torram/geometry/geometry.py
@@ -132,9 +132,9 @@ def rotation_matrix_to_angle_axis(rotation_matrix: torch.Tensor, epsilon: float 
     # Singularity handling.
     is_singular = (ymz.abs() < epsilon) & (xmz.abs() < epsilon) & (xmy.abs() < epsilon)
     is_no_rotation = (yz.abs() < epsilon) & (xz.abs() < epsilon) & (xy.abs() < epsilon) & ((trace - 3).abs() < epsilon)
-    xx = (rotation_matrix[..., 0, 0] + 1).unsqueeze(-1) / 2
-    yy = (rotation_matrix[..., 1, 1] + 1).unsqueeze(-1) / 2
-    zz = (rotation_matrix[..., 2, 2] + 1).unsqueeze(-1) / 2
+    xx = (rotation_matrix[..., 0, 0] + 1 + epsilon).unsqueeze(-1) / 2
+    yy = (rotation_matrix[..., 1, 1] + 1 + epsilon).unsqueeze(-1) / 2
+    zz = (rotation_matrix[..., 2, 2] + 1 + epsilon).unsqueeze(-1) / 2
     xy4 = xy / 4
     xz4 = xz / 4
     yz4 = yz / 4
@@ -166,6 +166,7 @@ def rotation_matrix_to_angle_axis(rotation_matrix: torch.Tensor, epsilon: float 
 
     # No singularity case. Normalize the off-diagonal entries to form the vector, with the angle
     # determined from the rotation matrice's trace.
+    trace = torch.clamp_min(trace, -1 + epsilon)
     s = torch.sqrt(ymz**2 + xmz**2 + xmy**2)
     angle = torch.acos((trace - 1) / 2)
     return torch.where(~is_singular, torch.cat([ymz, xmz, xmy], dim=-1) * angle / s, output)

torram/geometry/jacobians.py
@@ -66,11 +66,11 @@ def q3d_wrt_T(T: torch.Tensor, epsilon: float = 1e-4) -> torch.Tensor:
     J_sz = J_sz * factor_z.view(*batch_size, 1, 1, 1)
 
     # "Normal" transformation jacobian.
-    a = r00 + r11 + r22 - 1
+    a = torch.clamp(r00 + r11 + r22 - 1, -1 + epsilon, 1 - epsilon)
     b = torch.sqrt((r01 - r10)**2 + (r02 - r20)**2 + (r12 - r21)**2)
     c = torch.acos(a / 2)
-    norm1 = torch.sqrt(4 - a**2) * b
-    norm2 = b ** 3
+    norm1 = torch.sqrt(4 - a**2) * b + epsilon
+    norm2 = b ** 3 + epsilon
     J_n = torch.zeros((*batch_size, 3, 4, 4), dtype=T.dtype, device=T.device)
     J_n[..., 0, 0, 0] = (r12 - r21) / norm1
     J_n[..., 1, 0, 0] = (r20 - r02) / norm1
@@ -119,14 +119,14 @@ def q3d_wrt_T(T: torch.Tensor, epsilon: float = 1e-4) -> torch.Tensor:
     return torch.where(~is_singular, J_n, J)
 
 
-def T_wrt_q3d(q3d: torch.Tensor) -> torch.Tensor:
+def T_wrt_q3d(q3d: torch.Tensor, eps: float = 1e-6) -> torch.Tensor:
     """Jacobian of the transformation q3d (axis-angle vector) -> T (transformation matrix).
     Math inspired by: https://github.com/dorianhenning/ba-srl/blob/smpl-refactor/scripts/compute_smpl_jacobian.py
     """
     batch_size = q3d.shape[:-1]
     J = torch.zeros((*batch_size, 4, 4, 3), dtype=q3d.dtype, device=q3d.device)
 
-    phi = torch.norm(q3d, dim=-1, keepdim=True)
+    phi = torch.norm(q3d, dim=-1, keepdim=True) + eps
     phi2 = phi**2
     sin_t = torch.sin(phi)
     sinc_t = sin_t / phi