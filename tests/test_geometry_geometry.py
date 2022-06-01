fix: T_wrt_q3d jacobian with wrong clamping value
- factor a must be clamped to [-2, 2], not [-1, 1]
- fixed T_wrt_q3d and multiply_angle_axis unit tests (eps = 0)

tests/test_geometry_geometry.py
@@ -34,7 +34,7 @@ def test_inverse_quaternion(shape):
 def test_multiply_angle_axis(shape: Tuple[int, ...]):
     a = torch.rand(shape)
     b = torch.rand(shape)
-    c_hat = torram.geometry.multiply_angle_axis(a, b)
+    c_hat = torram.geometry.multiply_angle_axis(a, b, eps=0)
 
     Ra = torram.geometry.angle_axis_to_rotation_matrix(a)
     Rb = torram.geometry.angle_axis_to_rotation_matrix(b)
@@ -47,10 +47,6 @@ def test_multiply_angle_axis_zeros():
     b = torch.rand((4, 3))
     c_hat = torram.geometry.multiply_angle_axis(a, b)
     assert not torch.any(torch.isnan(c_hat))
-
-
-if __name__ == '__main__':
-    test_multiply_angle_axis_zeros()
 
 
 @pytest.mark.parametrize("shape", [(4, 3), (4, 1, 3, 3)])
@@ -71,7 +67,7 @@ def test_rotation_6d_and_angle_axis(shape):
 
 
 @pytest.mark.parametrize("shape", [(4, 3), (4, 1, 3, 3)])
-def test_rotation_matrix_against_kornia(shape):
+def test_rotation_matrix_to_angle_axis_against_kornia(shape):
     q3d = torch.rand(shape)
     rotation_matrix = torram.geometry.angle_axis_to_rotation_matrix(q3d)
 
@@ -96,11 +92,6 @@ def test_rotation_matrix_singularity_180_x():
     q3d_hat = torram.geometry.rotation_matrix_to_angle_axis(rotation_matrix, epsilon=1e-8)
     q3d_kornia = kornia.geometry.rotation_matrix_to_angle_axis(rotation_matrix)
     assert torch.allclose(q3d_hat, q3d_kornia)
-
-
-if __name__ == '__main__':
-    test_rotation_matrix_singularity_180_x()
-
 
 
 def test_rotation_matrix_singularity_180_y():

tests/test_geometry_jacobians.py
@@ -8,15 +8,41 @@ from torram.geometry import jacobians as torram_jacobians
 from typing import Tuple
 
 
-def get_random_transform(shape):
-    t = torch.rand((*shape, 3), dtype=torch.float64)
-    q = torch.rand((*shape, 4), dtype=torch.float64)
+def get_random_transform(shape, dtype=torch.float64):
+    t = torch.rand((*shape, 3), dtype=dtype)
+    q = torch.rand((*shape, 4), dtype=dtype)
     return pose_to_transformation_matrix(t, q)
 
 
 def T_from_q(q: torch.Tensor) -> torch.Tensor:
     t = torch.zeros((*q.shape[:-1], 3), dtype=q.dtype, device=q.device)
     return torram.geometry.pose_to_transformation_matrix(t, q)
+
+
+def zero_gradients(x):
+    if isinstance(x, torch.Tensor):
+        if x.grad is not None:
+            x.grad.detach_()
+            x.grad.zero_()
+
+
+def compute_jacobian(inputs, output):
+    assert inputs.requires_grad
+    num_classes = output.size()[1]
+
+    jacobian = torch.zeros(num_classes, *inputs.size())
+    grad_output = torch.zeros(*output.size())
+    if inputs.is_cuda:
+        grad_output = grad_output.cuda()
+        jacobian = jacobian.cuda()
+
+    for i in range(num_classes):
+        zero_gradients(inputs)
+        grad_output.zero_()
+        grad_output[:, i] = 1
+        output.backward(grad_output, retain_graph=True)
+        jacobian[i] = inputs.grad.data
+    return torch.transpose(jacobian, dim0=0, dim1=1)
 
 
 @pytest.mark.parametrize("shape", ((3, ), (8, 3), (1, 3), (5, 8, 3)))
@@ -87,18 +113,15 @@ def test_q4d_wrt_T(shape, delta: float = 1e-6):
             assert torch.allclose(J_hat[..., i, j], J, atol=1e-4)
 
 
-@pytest.mark.parametrize("shape", ((4, 4), (8, 4, 4), (1, 4, 4), (3, 4, 4), (5, 8, 4, 4)))
-def test_q3d_wrt_T(shape, delta: float = 1e-6):
-    x = get_random_transform(shape[:-2])
-    q = torram.geometry.rotation_matrix_to_angle_axis(x[..., :3, :3].contiguous())
-    J_hat = torram_jacobians.q3d_wrt_T(x)
-    for i in range(4):
-        for j in range(4):
-            x_ = x.clone()
-            x_[..., i, j] += delta
-            q_ = torram.geometry.rotation_matrix_to_angle_axis(x_[..., :3, :3].contiguous())
-            J = (q_ - q) / delta
-            assert torch.allclose(J_hat[..., i, j], J, atol=1e-4)
+def test_q3d_wrt_T():
+    """Numerical differentiation is hard here as the rotation matrix R is not valid after adding
+    an increment (norm != 1). Therefore, using autograd here."""
+    x = get_random_transform((1, ), dtype=torch.float32)
+    x.requires_grad = True
+    q = torram.geometry.rotation_matrix_to_angle_axis(x[..., :3, :3].contiguous(), epsilon=0)
+    J_hat = torram_jacobians.q3d_wrt_T(x, epsilon=0)
+    J_autograd = compute_jacobian(x, q)
+    assert torch.allclose(J_hat, J_autograd)
 
 
 @pytest.mark.parametrize("shape", ((4, 4), (8, 4, 4), (1, 4, 4), (5, 8, 4, 4)))

torram/geometry/geometry.py
@@ -106,7 +106,7 @@ def angle_axis_to_quaternion(angle_axis: torch.Tensor) -> torch.Tensor:
     return quaternion_flat.view(*shape, 4)
 
 
-def rotation_matrix_to_angle_axis(rotation_matrix: torch.Tensor, epsilon: float = 1e-4) -> torch.Tensor:
+def rotation_matrix_to_angle_axis(rotation_matrix: torch.Tensor, epsilon: float = 1e-6) -> torch.Tensor:
     """Convert 3x3 rotation matrix to axis angle representation.
     Math inspired by https://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToAngle/index.htm
 
@@ -167,7 +167,7 @@ def rotation_matrix_to_angle_axis(rotation_matrix: torch.Tensor, epsilon: float 
 
     # No singularity case. Normalize the off-diagonal entries to form the vector, with the angle
     # determined from the rotation matrice's trace.
-    trace = torch.clamp_min(trace, -1 + epsilon)
+    trace = torch.clamp(trace, -1 + epsilon, 1 - epsilon)
     s = torch.sqrt(ymz**2 + xmz**2 + xmy**2)
     angle = torch.acos((trace - 1) / 2)
     return torch.where(~is_singular, torch.cat([ymz, xmz, xmy], dim=-1) * angle / s, output)  # noqa

torram/geometry/jacobians.py
@@ -66,10 +66,10 @@ def q3d_wrt_T(T: torch.Tensor, epsilon: float = 1e-4) -> torch.Tensor:
     J_sz = J_sz * factor_z.view(*batch_size, 1, 1, 1)
 
     # "Normal" transformation jacobian.
-    a = torch.clamp(r00 + r11 + r22 - 1, -1 + epsilon, 1 - epsilon)
-    b = torch.sqrt((r01 - r10)**2 + (r02 - r20)**2 + (r12 - r21)**2)
+    a = torch.clamp(r00 + r11 + r22 - 1, -2 + epsilon, 2 - epsilon)
+    b = torch.sqrt((r01 - r10)**2 + (r02 - r20)**2 + (r12 - r21)**2) + epsilon
     c = torch.acos(a / 2)
-    norm1 = torch.sqrt(4 - a**2) * b + epsilon
+    norm1 = torch.sqrt(4 - a**2) * b
     norm2 = b ** 3 + epsilon
     J_n = torch.zeros((*batch_size, 3, 4, 4), dtype=T.dtype, device=T.device)
     J_n[..., 0, 0, 0] = (r12 - r21) / norm1