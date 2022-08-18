227dfc2015dd8770565c2e84ec3cc6ecc9a799e3
simon-schaefer <simon.k.schaefer@gmail.com>
2022-08-18 12:33:03 +0200

    fix: kalman filter tests fixed

--- tests/test_estimation_kalman.py
+++ tests/test_estimation_kalman.py
@@ -12,10 +12,10 @@ def test_equal_uncertainty(shape: Tuple[int, ...]):
     P_hat = torram.geometry.eye(shape)
     R = torram.geometry.eye(shape)
 
-    x = torram.estimation.kalman_update(x_hat, z, P_hat=P_hat, R=R)
-    assert torch.allclose(x.mean, (x_hat + z) / 2)
-    expected = torram.geometry.eye(shape) * 0.5
-    assert torch.allclose(x.covariance_matrix, expected, atol=1e-3)
+    x_f, P_f = torram.estimation.kalman_update(x_hat, z, P_hat=P_hat, R=R)
+    assert torch.allclose(x_f, (x_hat + z) / 2)
+    expected = torch.mul(0.5, torram.geometry.eye(shape))
+    assert torch.allclose(P_f, expected, atol=1e-3)
 
 
 @pytest.mark.parametrize("shape", ((1, ), (8, 2), (8, 1, 3)))
@@ -25,9 +25,9 @@ def test_z_certain(shape: Tuple[int, ...]):
     P_hat = torram.geometry.eye(shape) * 1000
     R = torram.geometry.eye(shape) * 0.1
 
-    x = torram.estimation.kalman_update(x_hat, z, P_hat=P_hat, R=R)
-    assert torch.allclose(x.mean, z, atol=1e-3)
-    assert torch.allclose(x.covariance_matrix, R, atol=1e-3)
+    x_f, P_f = torram.estimation.kalman_update(x_hat, z, P_hat=P_hat, R=R)
+    assert torch.allclose(x_f, z, atol=1e-3)
+    assert torch.allclose(P_f, R, atol=1e-3)
 
 
 @pytest.mark.parametrize("shape", ((1, ), (8, 2), (8, 1, 3)))
@@ -37,6 +37,6 @@ def test_x_certain(shape: Tuple[int, ...]):
     P_hat = torram.geometry.eye(shape) * 0.1
     R = torram.geometry.eye(shape) * 1000
 
-    x = torram.estimation.kalman_update(x_hat, z, P_hat=P_hat, R=R)
-    assert torch.allclose(x.mean, x_hat, atol=1e-3)
-    assert torch.allclose(x.covariance_matrix, P_hat, atol=1e-3)
+    x_f, P_f = torram.estimation.kalman_update(x_hat, z, P_hat=P_hat, R=R)
+    assert torch.allclose(x_f, x_hat, atol=1e-3)
+    assert torch.allclose(P_f, P_hat, atol=1e-3)
