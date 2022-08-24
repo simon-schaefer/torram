04cb3d797d5e9f843bbdb8b9b1fc6fdeedd13951
simon-schaefer <simon.k.schaefer@gmail.com>
2022-08-24 15:31:16 +0200

    further simplification
    - deleted config -> omegaconf
    - deleted evaluator factory -> pytorch lightning

--- torram/utility/__init__.py
+++ torram/utility/__init__.py
@@ -1,7 +1,5 @@
 import torram.utility.logger
 import torram.utility.moving
 
-from torram.utility.config import Config
-from torram.utility.evaluator_factory import EvaluatorFactory
 from torram.utility.tensor_tuple import TensorTuple
 from torram.utility.timer import Timer
