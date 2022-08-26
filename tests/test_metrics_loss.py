490d40926a2160aa044ccab83f74a1011d998e05
simon-schaefer <simon.k.schaefer@gmail.com>
2022-08-26 15:23:54 +0200

    contrastive loss

--- tests/test_metrics_loss.py
+++ tests/test_metrics_loss.py
@@ -14,3 +14,11 @@ def test_full_nll_loss_against_torch():
     assert torch.allclose(loss_hat, loss)
     loss_hat_marginalized = torram.metrics.full_nll_loss(mean, target, covariance, True, reduction='sum')
     assert torch.allclose(loss_hat_marginalized, loss)
+
+
+def test_contrastive_loss():
+    y_hat = torch.arange(10, dtype=torch.float32)
+    y_hat[4] = 10.0
+    target = torch.arange(10)
+    loss = torram.metrics.contrastive_loss(y_hat, target, reduction='sum')
+    assert float(loss) == (5 + 4 + 3 + 2 + 1)  # all elements above 4 => 10 - 5, 10 - 6, ..., 10 -9
--- torram/metrics/loss.py
+++ torram/metrics/loss.py
@@ -1,4 +1,10 @@
+import itertools
+import numpy as np
 import torch
+
+
+__all__ = ['full_nll_loss',
+           'contrastive_loss']
 
 
 def full_nll_loss(mean: torch.Tensor, target: torch.Tensor, covariance: torch.Tensor,
@@ -38,3 +44,40 @@ def full_nll_loss(mean: torch.Tensor, target: torch.Tensor, covariance: torch.Te
     else:
         raise ValueError(f"Unknown reduction method {reduction}, choose from [mean, sum]")
     return result
+
+
+def contrastive_loss(
+    y_hat: torch.Tensor,
+    anchors: torch.Tensor,
+    anchor_margin: float = 0.0,
+    margin: float = 0,
+    reduction: str = 'mean'
+) -> torch.Tensor:
+    """Compute the contrastive loss (also ranking loss) of the prediction `y_hat` given a target for the
+    prediction's ordering. The contrastive loss targets the following ordering:
+
+        y_hat[i] > y_hat[j]     if anchors[i] > anchors[j]
+        y_hat[j] >= y_hat[i]    otherwise
+
+    Args:
+        y_hat: model predictions (N,).
+        anchors: reference values for prediction ordering (N,).
+        anchor_margin: minimal difference of the anchors to be included.
+        margin: minimal difference of the predictions to be included.
+        reduction: specifies the reduction to apply to the output: 'none' | 'mean' | 'sum'.
+    """
+    if y_hat.shape != anchors.shape:
+        raise ValueError(f"Non-Matching shape of y_hat and anchors, got {y_hat.shape} and {anchors.shape}")
+    if len(y_hat.shape) != 1:
+        raise ValueError(f"Expected flat input tensor, got {y_hat.shape}")
+    pairs_ij = np.array([ij for ij in itertools.combinations(list(range(len(y_hat))), 2)])
+    labels_ij = torch.tensor([1 if anchors[i] > anchors[j] else -1 for i, j in pairs_ij])
+    is_included = [k for k, (i, j) in enumerate(pairs_ij) if abs(anchors[i] - anchors[j]) > anchor_margin]
+
+    return torch.nn.functional.margin_ranking_loss(
+        input1=y_hat[pairs_ij[is_included, 0]],
+        input2=y_hat[pairs_ij[is_included, 1]],
+        target=labels_ij[is_included],
+        margin=margin,
+        reduction=reduction
+    )
