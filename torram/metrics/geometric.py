2bc8bb7d3985e3c3ff83a1f68fd28d80ed1d9013
simon-schaefer <simon.k.schaefer@gmail.com>
2022-08-29 17:12:31 +0200

    further simplification
    - deleted l1 quaternion loss, not entirely  sure about validity

--- torram/metrics/geometric.py
+++ torram/metrics/geometric.py
@@ -3,8 +3,7 @@ from typing import Optional
 
 
 __all__ = ['euclidean_distance',
-           'geodesic_loss',
-           'l1_quaternion']
+           'geodesic_loss']
 
 
 def euclidean_distance(x_hat: torch.Tensor, x: torch.Tensor) -> torch.Tensor:
@@ -40,23 +39,3 @@ def geodesic_loss(x_hat: torch.Tensor, x: Optional[torch.Tensor] = None, eps: fl
         R_diffs = x_hat  # no "back"-rotation
     traces = R_diffs.diagonal(dim1=-2, dim2=-1).sum(-1)
     return torch.acos(torch.clamp((traces - 1) / 2, -1 + eps, 1 - eps))
-
-
-def l1_quaternion(x_hat: torch.Tensor, x: torch.Tensor):
-    """L1-like quaternion loss function, adapted from:
-    https://towardsdatascience.com/better-rotation-representations-for-accurate-pose-estimation-e890a7e1317f
-
-    Args:
-        x_hat: predicted rotation in quaternion (B, N, 4).
-        x: target rotation in quaternion (B, N, 4).
-    """
-    if not x_hat.ndim == x.ndim == 3 or not x_hat.shape[-1] == x.shape[-1] == 4:
-        raise ValueError(f"Invalid quaternion shape, expected (B, N, 4), got {x.shape} and {x_hat.shape}")
-    if x.shape != x_hat.shape:
-        raise ValueError(f"Non matching prediction and target, got {x.shape} and {x_hat.shape}")
-
-    x_hat_normed = x_hat / torch.norm(x_hat, dim=-1, keepdim=True)
-    l1_plus = torch.norm(x + x_hat_normed, p=1, dim=-1)
-    l1_minus = torch.norm(x - x_hat_normed, p=1, dim=-1)
-    loss = torch.min(l1_minus, l1_plus)
-    return torch.mean(loss, dim=-1)
