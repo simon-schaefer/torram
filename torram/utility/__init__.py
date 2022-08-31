70d56b6c9d3b16dedb5e546d28ce0a08c7aece58
simon-schaefer <simon.k.schaefer@gmail.com>
2022-08-31 15:28:14 +0200

    simplified: removed all loggers

--- torram/utility/__init__.py
+++ torram/utility/__init__.py
@@ -1,5 +1,2 @@
-import torram.utility.logger
-import torram.utility.moving
-
 from torram.utility.tensor_tuple import TensorTuple
 from torram.utility.timer import Timer
--- torram/utility/logger/__init__.py
+++
@@ -1,3 +0,0 @@
-from torram.utility.logger.image_logger import ImageLogger
-from torram.utility.logger.log_logger import LogLogger
-from torram.utility.logger.tensorboardy import TensorboardY
