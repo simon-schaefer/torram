3a2711fa21739aa341d6cfbb362d247f5523bb51
simon-schaefer <simon.k.schaefer@gmail.com>
2022-09-08 08:48:41 +0200

    fix: marginalize_distribution() should return variances, not stddev

--- torram/geometry/uncertainty.py
+++ torram/geometry/uncertainty.py
@@ -2,12 +2,12 @@ import torch
 import torram
 
 
-def marginalize_distribution(cov: torch.Tensor, min_variance: float = 0.01, return_stddev: bool = False
+def marginalize_distribution(cov: torch.Tensor, min_variance: float = 0.01, return_variance: bool = False
                              ) -> torch.Tensor:
     if min_variance <= 0:
         raise ValueError(f"Invalid minimal variance, expected > 0, got {min_variance}")
     var_marginal = torch.diagonal(cov, dim1=-2, dim2=-1)
-    stddev_marginal = torch.sqrt(torch.clamp_min(var_marginal, min_variance))
-    if return_stddev:
-        return stddev_marginal
-    return torram.geometry.diag_last(stddev_marginal)
+    var_marginal = torch.clamp_min(var_marginal, min_variance)
+    if return_variance:
+        return var_marginal
+    return torram.geometry.diag_last(var_marginal)
